c1e8959cd424ffadf936bb410f8066f3b4a1abf6
Zaky97 <zaky7645@gmail.com>
2023-12-29 19:58:59 +0700

    new file:   Javascript/dasar.js

---
+++ Javascript/dasar.js
@@ -0,0 +1,45 @@
+// Membuat Sebuah Variabel
+
+var a = "a";
+let b = "b" ;
+const c = "c";
+
+// Tipe Data
+
+let nama = "Zaky"; // string
+let umur = 17; // number int
+let Yes_or_No = true // Boolean
+let Desimal = 95.9 // float
+
+// Operator Aritmatika
+
+let n1 = 20;
+let n2 = 10;
+console.log(n1 + n2, "Penjumlahan"); // Penjumlahan
+console.log(n1 - n2, "Pengurangan"); // Pengurangan
+console.log(n1 * n2, "Perkalian"); // Perkalian
+console.log(n1 / n2, "Pembagian"); // Pembagian
+console.log(n1 % n2, "Modulus"); // Hasil Bagi / Modulus
+
+// Mengisi Value Pada Variable
+
+let Operasi_Aritmatika = n1 + n2;
+console.log(Operasi_Aritmatika); // 30
+
+Operasi_Aritmatika = n1 - n2;
+console.log(Operasi_Aritmatika); // 10
+
+Operasi_Aritmatika = n1 * n2;
+console.log(Operasi_Aritmatika); // 200
+
+Operasi_Aritmatika = n1 / n2;
+console.log(Operasi_Aritmatika); // 2
+
+Operasi_Aritmatika = n1 % n2;
+console.log(Operasi_Aritmatika); // 0
+
+// Aritmatika Pada Tipe Data String
+let firstname = "Muhammad";
+let lastname = "Zaky";
+let fullname = firstname + lastname;
+console.log(fullname);
